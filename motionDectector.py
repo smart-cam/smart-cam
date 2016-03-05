4cd6c4520d8c9aa468745524fd4b58e45685979d
James <james@bla.com>
2016-03-05 16:51:58 -0500

    added S3 upload functionality

--- motionDectector.py
+++ motionDectector.py
@@ -4,9 +4,13 @@ from scipy import ndimage
 from scipy.spatial import distance
 import time
 import math
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import subprocess
 
 RASPBERRY_PI_NAME = "Raspberry_PI_1"
-CAMERA_INDEX = 0        # Usually 0 for webcam
+CAMERA_INDEX = 0        # Usually 0 for webcam, not needed for Pi camera
+DEST_S3_BUCKET = "s3://w210-video-repo"
 
 # Motion Detection class
 class MotionDetector:
@@ -15,17 +19,22 @@ class MotionDetector:
         self.previous_center_of_mass = (0, 0)
         self.previous_sum_of_edges = 0
 
+    ### NOTE: do we need to pass frame to this function? it is unused
     def isMotion(self, frame, edges):
         # Converting edges from 255 to 1
         edges_to_1 = edges/255
-        
-        # Calculating distance between center of mass between 2 consecutive frames
-        current_center_of_mass = ndimage.measurements.center_of_mass(edges_to_1)
-        distance_of_centers = distance.euclidean(current_center_of_mass, self.previous_center_of_mass)
+       
+        try:
+            # Calculating distance between center of mass between 2 consecutive frames
+            current_center_of_mass = ndimage.measurements.center_of_mass(edges_to_1)
+            distance_of_centers = distance.euclidean(current_center_of_mass, self.previous_center_of_mass)
 
-        # Calculating change in sum of edges
-        current_sum_of_edges = int(edges_to_1.sum())
-        change_in_sum_of_edges = abs(current_sum_of_edges - self.previous_sum_of_edges)
+            # Calculating change in sum of edges
+            current_sum_of_edges = int(edges_to_1.sum())
+            change_in_sum_of_edges = abs(current_sum_of_edges - self.previous_sum_of_edges)
+
+        except ValueError:
+            return False
 
         # Assign current values to previous values
         self.previous_center_of_mass = current_center_of_mass
@@ -58,9 +67,11 @@ def writeToFrame(frame, edges):
 # Video Clip Writer class
 class VideoClipWriter:
     def __init__(self):
-        self.fourcc = cv2.cv.CV_FOURCC(*'XVID')
+        #self.fourcc = cv2.cv.CV_FOURCC(*'XVID')
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
         self.filename_prefix = RASPBERRY_PI_NAME
         self.filename_timestamp = 0
+        self.filename = ""
         self.motion_detection_timestamp = 0
         self.clipWriter = None
         self.videoLength = 60        # in seconds
@@ -78,38 +89,54 @@ class VideoClipWriter:
             elif self.clipWriter.isOpened() == True:
                 self.motion_detection_timestamp = curr_time
     
+    # return the filename from this method if we're done writing the file
     def frameTrigger(self, frame):
         curr_time = time.time()
+        retval = ""
         if self.clipWriter == None:
-            return
+            return retval
+
         if self.clipWriter.isOpened() == True and curr_time - self.filename_timestamp < self.videoLength and curr_time - self.motion_detection_timestamp < self.record_after_motion_detection:
             self.clipWriter.write(frame)
+
         elif self.clipWriter.isOpened() == True and curr_time - self.filename_timestamp > self.videoLength and curr_time - self.motion_detection_timestamp < self.record_after_motion_detection:
             self.clipWriter.release()
             self.clipWriter = None
             self.createVideoWriterObject(curr_time)
             self.filename_timestamp = curr_time
+
         elif self.clipWriter.isOpened() == True and curr_time - self.filename_timestamp < self.videoLength and curr_time - self.motion_detection_timestamp > self.record_after_motion_detection:
             self.clipWriter.release()
             self.clipWriter = None
-        return
+            retval = self.filename
+
+        return retval
 
     def createVideoWriterObject(self, curr_time):
         filename = self.filename_prefix + '_' + str(curr_time) + '.avi'
-        self.clipWriter = cv2.VideoWriter(filename, self.fourcc, 20.0, (640,480))
+        self.filename = filename
+        self.clipWriter = cv2.VideoWriter(filename, self.fourcc, 16.0, (640,480))
 
 def main():
     # Creating video capture object
-    camera = cv2.VideoCapture(CAMERA_INDEX)
+    #camera = cv2.VideoCapture(CAMERA_INDEX)
+
+    # set up the Pi's camera
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 16
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+
     # Creating motion detector object
     motionDetectorObject = MotionDetector()
     # Creating video writer object
     clipWriterObject = VideoClipWriter()
 
-    while True:
-        # Capturing frame
-        ret, frame = camera.read()
+    # Capture frames
+    for frame_data in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        #ret, frame = camera.read()
         # Getting edges
+        frame = frame_data.array
         edges = cv2.Canny(frame, 160, 200)
 
         isMotion = motionDetectorObject.isMotion(frame, edges)
@@ -118,13 +145,25 @@ def main():
         # Writing to frame and edges
         frame, edges = writeToFrame(frame, edges)
         clipWriterObject.motionTrigger(isMotion)
-        clipWriterObject.frameTrigger(frame)
+        retval = clipWriterObject.frameTrigger(frame)
+
+        # if video file written, upload to S3 in background process
+        if retval:
+            print "uploading file %s" %retval
+            subprocess.Popen(["aws", "s3", "cp", retval, DEST_S3_BUCKET + '/' + retval])
         
         cv2.imshow("Frame", frame)
         cv2.imshow("Edges", edges)
-        cv2.waitKey(1)
+        key = cv2.waitKey(1) & 0xFF
 
-    camera.release()
+        # clear stream for next frame capture
+        rawCapture.truncate(0)
+
+        # end capture if q key is pressed
+        if key == ord('q'):
+            break
+
+    #camera.release()
 
 if __name__ == '__main__':
     main()
