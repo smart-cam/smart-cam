ac60490b029f0e802eb26e04da8787ec18c0556d
sayantansatpati <sayantan.satpati.sfbay@gmail.com>
2016-04-03 22:04:01 -0700

    Fix

--- imageproc/face_detection_test.py
+++ imageproc/face_detection_test.py
@@ -1,20 +1,21 @@
 __author__ = 'ssatpati'
 from face_detection import FaceDetection
 from util.db import DynamoDBUtils
-import pprint
 from util import misc
+import shutil
+import pprint
 import os
 
+OUTPUT_DIR = '../videos'
 
 if __name__ == '__main__':
     BUCKET_NAME = 'smart-cam'
 
-    try:
-        os.remove('../videos/video_1.avi')
-    except Exception as e:
-        print e
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    os.makedirs(OUTPUT_DIR)
 
-    ret, local_file = misc.download_from_s3(BUCKET_NAME, 'videos/video_1.avi', '../videos')
+    ret, local_file = misc.download_from_s3(BUCKET_NAME, 'videos/video_1.avi', OUTPUT_DIR)
 
     print ret, local_file
 
