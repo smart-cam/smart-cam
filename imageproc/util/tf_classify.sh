ffb78fd960357663452676937f7a794a73e3561b
sayantansatpati <sayantan.satpati.sfbay@gmail.com>
2016-04-18 20:11:40 -0700

    More changes for testing on Ubuntu

--- imageproc/util/tf_classify.sh
+++ imageproc/util/tf_classify.sh
@@ -13,7 +13,7 @@ cd ~/tensorflow
 bazel-bin/tensorflow/examples/label_image/label_image \
 --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
 --output_layer=final_result \
---image= ${image_file} > ${out_file}_tmp 2>&1
+--image=${image_file} > ${out_file}_tmp 2>&1
 
 # Create Output File
 cat ${out_file}_tmp | grep ^I | awk -F'[\]]' '{print $2}' | awk '{$1=$1};1' > $out_file
