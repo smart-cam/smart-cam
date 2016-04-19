81a461f39247de000d8d0c159b5e4c5eb978a95c
sayantansatpati <sayantan.satpati.sfbay@gmail.com>
2016-04-18 20:00:23 -0700

    More changes for testing on Ubuntu

--- imageproc/util/tf_classify.sh
+++ imageproc/util/tf_classify.sh
@@ -7,15 +7,16 @@ echo "Classifying Image File: ${image_file}"
 echo "Output File: ${out_file}"
 
 # Change Directory
-#cd ~/tensorflow
+cd ~/tensorflow
 
-# Run Tensorflow
-#bazel build tensorflow/examples/label_image:label_image && \
-#bazel-bin/tensorflow/examples/label_image/label_image \
-#--graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
-#--output_layer=final_result \
-#--image=$HOME/images/test/frame_1.png
+# Run Tensorflow & dump to a temp file
+bazel-bin/tensorflow/examples/label_image/label_image \
+--graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
+--output_layer=final_result \
+--image= ${image_file} > ${out_file}_tmp 2>&1
 
 # Create Output File
-cat /Users/ssatpati/0-DATASCIENCE/DEV/github/smart-cam/resources/tf.out | \
-        grep ^I | awk -F'[\]]' '{print $2}' | awk '{$1=$1};1' > $out_file
+cat ${out_file}_tmp | grep ^I | awk -F'[\]]' '{print $2}' | awk '{$1=$1};1' > $out_file
+
+# Remove Temp File
+rm ${out_file}_tmp
