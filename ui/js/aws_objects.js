552fcee55d4b8f7f3ae37f0d3eb55ad6f438240f
Vineet Gangwar <vineet.gangwar@gmail.com>
2016-04-28 20:47:47 -0400

    Cleanup

--- ui/js/aws_objects.js
+++
@@ -1,6 +0,0 @@
-// Setting AWS Dynamo Credentials
-AWS.config.update({accessKeyId: 'AKIAIDNNNRKSEKJB6FXQ', secretAccessKey: '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'});
-var s3 = new AWS.S3();
-
-AWS.config.region = 'us-west-1';
-var table = new AWS.DynamoDB({params: {TableName: 'SMARTCAM'}});
